728d9447e4594d206dd74f46753bbd6b6d6e01a0
3mpirical <3mpirical10510@gmail.com>
2019-02-26 17:12:52 -0700

    added initial stying for header and navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_patient!
+    before_action :authenticate_doctor!
+    before_action :configure_permitted_params, if: :devise_controller?
+
+    private
+        def configure_permitted_params
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+        end
+
 end
